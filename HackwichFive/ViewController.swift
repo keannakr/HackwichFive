fb8eb73c166ac0e72e2a60a12517133e76c86faa
Keanna <keannakr@hawaii.edu>
2019-03-25 17:29:23 -1000

    Finished Hackwich #5 Part 3 & 4

--- HackwichFive/ViewController.swift
+++ HackwichFive/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
 
     @IBAction func sliderHasMoved(_ sender: Any) {
         
+        
         print("The value of the slider is:\(slider.value)")
         currentValue = lroundf(slider.value)
     }
@@ -21,15 +22,39 @@ class ViewController: UIViewController {
     //Create a variable that can hold the value of the slider
     var currentValue: Int = 0
     
+    var targetValue: Int = 0
+    
+    @IBOutlet var targetLabel: UILabel!
+    
+    func updateTargetLabel() {
+        targetLabel.text = String(targetValue)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        currentValue = lroundf(slider.value)
+        
+        targetValue = Int.random(in: 0...10)
+        
+        let thumbImageNormal = UIImage (named:"SliderThumb-Normal")
+        slider.setThumbImage(thumbImageNormal, for: .normal)
+    }
+    
+    func startNewRound() {
+        
+        targetValue = Int.random(in: 0...10)
+        currentValue = 50
+        currentValue = lroundf(slider.value)
+        
+        updateTargetLabel()
     }
 
 
     @IBAction func myGuessButtonPressed(_ sender: UIButton) {
     //5.New variable message that displays a message that includes currentValue
-        let message = "The value is \(currentValue)"
+        let message = "The value is \(currentValue)" + "\nThe Target Value is: \(targetValue)"
         
     //1.Create alert view
         let alert = UIAlertController (title:"Hello World!", message: message, preferredStyle: .alert)
@@ -43,6 +68,7 @@ class ViewController: UIViewController {
     //4.Present alertview on screen
         present(alert, animated: true, completion: nil)
     
+        startNewRound()
 }
     
 }
